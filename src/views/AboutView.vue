f8757890ce32093a5284cef34416befc17b667de
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-10-04 16:03:09 +0200

    first commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <video autoplay src="../assets/about-video.mp4" controls loop> </video>
+    <button @click="loadfunc()" >button</button>
+    <audio autoplay controls></audio>
   </div>
-</template>
+},
+methods:{
+  loadfunc(){
+    navigator.mediaDevices.getUserMedia({
+      video:true, 
+      audio:true
+    }).then(stream=>{
+      document.querySelector('video').srcObject=stream 
+      document.querySelector('audio').srcObject=stream
+      console.log(stream)
+    })
+  }
+}
+}
+</script>
